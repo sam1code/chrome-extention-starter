1838a5f2b02097f6249b5f41dad2c219bb5d2c25
sam-streak <soumyakanta.pattanaik@streak.tech>
2024-03-15 16:41:28 +0100

    changed something

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,62 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import logo from "./assets/react.svg";
+import { Box, Button } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const [documentTitle, setDocumentTitle] = useState("React App");
+  const [data, setData] = useState({});
 
   useEffect(() => {
-    chrome.tabs.query(
-      { active: true, currentWindow: true },
-      (tabs: chrome.tabs.Tab[]) => {
-        chrome.tabs.sendMessage(tabs[0]?.id as number, {
-          action: "changeTitle",
-          title: "New Title",
-        });
-      }
-    );
-  }, [documentTitle]);
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      console.log(tabs);
+      setData(tabs[0]);
+    });
+  }, []);
 
   return (
-    <>
-      <img src={logo} alt="React logo" style={{ width: "100px" }} />
-      <h1>Counter</h1>
-      <p>{count}</p>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          gap: "1rem",
-        }}
-      >
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-        <button onClick={() => setCount(count - 1)}>Decrement</button>
-      </div>
+    <Box>
+      {data.favIconUrl && (
+        <img
+          src={data.favIconUrl}
+          alt="Favicon"
+          style={{ width: "50px", height: "50px" }}
+        />
+      )}
+      <h1>Hello, From Burger Beats!</h1>
+      <p>
+        You are currently on: <br />
+        {(() => {
+          switch (true) {
+            case (data as { url?: string }).url?.includes(
+              "bx=COO.1.2310.1.3236&venv_view=COO.1.1001.1.439101"
+            ):
+              return "Home Page";
+            case (data as { url?: string }).url?.includes(
+              "bx=COO.1.2310.1.3239&venv_view=COO.15.1001.1.204008"
+            ):
+              return "DMS";
+            case (data as { url?: string }).url?.includes(
+              "bx=COO.1.2310.1.3234&venv_view=COO.1.1.1.1693"
+            ):
+              return "Desktop";
+            case (data as { url?: string }).url?.includes(
+              "bx=COO.1.2310.1.3232&venv_view=COO.1.1.1.333"
+            ):
+              return "Schreibtisch";
+            default:
+              return (data as { url?: string }).url;
+          }
+        })()}
+      </p>
 
-      <button onClick={() => setDocumentTitle("New Title")}>
-        Change Document Title
-      </button>
-    </>
+      <Button
+        variant="contained"
+        color="info"
+        onClick={() => window.open("https://t.me/BurgerBeat_bot")}
+      >
+        Click me to use telebot
+      </Button>
+    </Box>
   );
 }
 
